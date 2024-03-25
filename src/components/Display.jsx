1c8e8ee497f7b27fba90550652362eb0344d1fbb
pratikbhor95 <pratikbhor95@gmail.com>
2024-03-25 02:04:20 +0000

    setup gh-pages

---
+++ src/components/Display.jsx
@@ -0,0 +1,16 @@
+import styles from "./Display.module.css";
+
+const Display = ({ displayValue }) => {
+  return (
+    <>
+      <input
+        type="text"
+        className={styles.display}
+        value={displayValue}
+        readOnly
+      ></input>
+    </>
+  );
+};
+
+export default Display;
